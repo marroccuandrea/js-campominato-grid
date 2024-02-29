ba8695e887786563e144480ef035ad3e5ad9e5a8
marroccuandrea <marroccuandrea@yahoo.it>
2024-02-29 16:46:05 +0100

    [add] numerazione quadrati

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 const grid = document.querySelector('.grid');
 const btnStart = document.querySelector('.btn-start');
 
-// Aggiungo la funzione che genera 100 quadrati 
+reset();
+
+// Aggiungo la funzione che genera 100 quadrati
 
     for(let i = 1; i <= 100; i++){
         const square = getSquare(i);
@@ -12,8 +14,14 @@ const btnStart = document.querySelector('.btn-start');
 function getSquare(indice){
     const square = document.createElement('div');
     square.className = 'square';
+    square.innerHTML = indice;
     square.sqID = indice;
 
 
     return square;
+}
+
+// Funzione reset
+function reset(){
+    grid.innerHTML = '';
 }
